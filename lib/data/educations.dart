1033611034f87761e1d91f44c3b691c473127812
Elif Özcan <ozcann159@gmail.com>
2024-08-02 11:24:16 +0300

    education editing

--- lib/data/educations.dart
+++ lib/data/educations.dart
@@ -2,23 +2,23 @@ import 'package:portfolio_app/models/education.dart';
 
 const List<Education> educations = [
   Education(
-    description: 'Master in Artificial Intelligence',
-    linkName: 'heyflutter.com',
-    period: '2020 - PRESENT',
+    description: 'Kamu Yönetimi',
+    linkName: 'Afyon Kocatepe Üniversitesi',
+    period: '2014- 2019',
   ),
   Education(
-    description: 'MBA E-Sports Management (Online)',
-    linkName: 'heyflutter.com',
-    period: '2018 - 2020',
+    description: 'Anadolu Üniversitesi (Online)',
+    linkName: 'Bilgisayar Programcılığı',
+    period: '2021 - 2023',
   ),
-  Education(
-    description: 'Bachelor\'s in Business Administration(Online)',
-    linkName: 'heyflutter.com',
-    period: '2017 - 2020',
-  ),
-  Education(
-    description: 'Bachelor\'s in Computer Science ',
-    linkName: 'heyflutter.com',
-    period: '2016 - 2019',
-  ),
+  // Education(
+  //   description: 'Bachelor\'s in Business Administration(Online)',
+  //   linkName: 'heyflutter.com',
+  //   period: '2017 - 2020',
+  // ),
+  // Education(
+  //   description: 'Bachelor\'s in Computer Science ',
+  //   linkName: 'heyflutter.com',
+  //   period: '2016 - 2019',
+  // ),
 ];
